3d6eafb0ab7494dc6006550e89a0a424404d1716
Bijin Kandengala <kbijin528.pro@gmail.com>
2024-12-15 17:56:23 +0530

    Update axios timeout from 5000ms to 10000ms

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -9,7 +9,7 @@ const baseURL =
 
 export const axiosInstance = axios.create({
   baseURL,
-  timeout: 5000,
+  timeout: 10000,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
